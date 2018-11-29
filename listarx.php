393756396cb1c9f0e083ba880056c1048ea53854
mauricioaschmitz <mauricioantonioli@gmail.com>
2018-11-29 16:28:09 -0200

    Adicionado ordenamento por data para as Imagens

--- listarx.php
+++ listarx.php
@@ -4,62 +4,58 @@ include_once "config.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="icon" href="favicon.ico">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="favicon.ico">
 
-  <title>Lista de Exames</title>
+        <title>Lista de Exames</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="style/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="style/dist/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="style/css/inserir.css" rel="stylesheet">
-</head>
-<?php
-include_once('style/include/head.php');
-$path = $_GET['paciente'];
-$diretorio = dir($path);
-?>
+        <!-- Custom styles for this template -->
+        <link href="style/css/inserir.css" rel="stylesheet">
+    </head>
+    <?php
+    include_once('style/include/head.php');
+    $path = $_GET['paciente'];
+    $diretorio = dir($path);
+    ?>
 
-<div class="container">
+    <div class="container">
 
-  <div class="starter-template">
+        <div class="starter-template">
 
-    <div class="row">
-      <div class="col-lg-12" style="text-align: left;">
-        <?php
-        $cont = 0;
-        while($arquivo = $diretorio -> read()){
-          $ext = explode(".",$arquivo);
+            <div class="row">
+                <div class="col-lg-12" style="text-align: left;">
+                    <?php
+                    //$dir = 'images/demo/';
+                    $files = scandir($path);
+                    rsort($files);
+                    foreach ($files as $file) {
+                        if ($file != '.' && $file != '..') {
+                            ?>
+                            <div class="col-lg-2 img-single column column-block">
+                                <a data-open = "galleryModal">
+                                    <a href = "<?php echo $path . '/' . $file; ?>" target = "_blank"><img src = "<?php echo $path . '/' . $file; ?>" class = "thumbnail center-block" alt = "Image with object-fit"></a>
+                                </a>
+                            </div>
+                            <?php
+                        }
+                    }
+                    ?>
 
-          if($cont >= 2 && $ext[1] == "jpg" || $ext[1] == "JPG"){
-            ?>
-            <div class="col-lg-3 img-single column column-block">
-              <a data-open="galleryModal">
-                <a href ="<?php echo $path.'/'.$arquivo; ?>" target="_blank"><img src="<?php echo $path.'/'.$arquivo; ?>" class="thumbnail center-block" alt="Image with object-fit"></a>
-              </a>
+                </div>
             </div>
 
-            <?php
-            
-          }
-          $cont++;
-        }
-        $diretorio -> close();
-        ?>
+        </div>
 
-      </div>
-    </div>
-
-  </div>
-
-</div><!-- /.container -->
+    </div><!-- /.container -->
 
 
     <!-- Bootstrap core JavaScript
@@ -72,7 +68,7 @@ $diretorio = dir($path);
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="style/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
     <script type="text/javascript">
-      $('.img-single').height($('.img-single').width());
+        $('.img-single').height($('.img-single').width());
     </script>
-  </body>
-  </html>
+</body>
+</html>
